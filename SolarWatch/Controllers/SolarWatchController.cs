6f4105a2fa64edcef255a4e26fc2e1be04a27f90
qeed97 <qeed97@gmail.com>
2024-08-24 13:34:58 +0200

    Created a controller and sollarforecast class

---
+++ SolarWatch/Controllers/SolarWatchController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SolarWatch.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SolarWatchController : ControllerBase
+{
+    private readonly ILogger<SolarWatchController> _logger;
+
+    public SolarWatchController(ILogger<SolarWatchController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("GetSolarWatch")]
+    public ActionResult<SolarForecast> Get(DateTime date, string city)
+    {
+        var forecast = new SolarForecast
+        {
+            Date = DateOnly.FromDateTime(date),
+            City = city,
+            Sunrise = new TimeOnly(6, 30, 45),
+            SunSet = new TimeOnly(20, 21, 32)
+        };
+
+        return Ok(forecast);
+    }
+}
---
+++ SolarWatch/SolarForecast.cs
@@ -0,0 +1,12 @@
+﻿namespace SolarWatch;
+
+public class SolarForecast
+{
+    public DateOnly Date { get; set; }
+
+    public TimeOnly Sunrise { get; set; }
+
+    public TimeOnly SunSet { get; set; }
+    
+    public string City { get; set; }
+}
