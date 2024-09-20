0fdc12c88c986e9e2abb7eb61bf1473d27bbd614
qeed97 <qeed97@gmail.com>
2024-09-21 00:10:21 +0200

    created solardata repository

---
+++ SolarWatch/Services/SolarDataServices/Repository/SolarDataRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarWatch.Data;
+using SolarWatch.Models;
+
+namespace SolarWatch.Services.SolarDataServices.Repository;
+
+public class SolarDataRepository(SolarDbContext context) : ISolarDataRepository
+{
+    public IEnumerable<SolarData> GetSolarData()
+    {
+        return context.SolarDatas
+            .Include(s => s.City);
+    }
+
+    public Task<SolarData?> GetSolarDataById(int id)
+    {
+        return context.SolarDatas
+            .Include(s => s.City)
+            .FirstOrDefaultAsync(s => s.Id == id);
+    }
+
+    public SolarData CreateSolarData(SolarData solarData, City city)
+    {
+        city.SolarData.Add(solarData);
+        context.SolarDatas.Add(solarData);
+        context.SaveChanges();
+        return solarData;
+    }
+
+    public void DeleteSolarData(SolarData solarData, City city)
+    {
+        city.SolarData.Remove(solarData);
+        context.SolarDatas.Remove(solarData);
+        context.SaveChanges();
+    }
+
+    public SolarData UpdateSolarData(SolarData solarData)
+    {
+        context.Update(solarData);
+        context.SaveChanges();
+        return solarData;
+    }
+}
