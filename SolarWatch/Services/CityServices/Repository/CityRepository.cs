c87e24644c93a4b68a03690a93d6aa57d77cb3a2
qeed97 <qeed97@gmail.com>
2024-09-20 23:57:00 +0200

    city repository created

---
+++ SolarWatch/Services/CityServices/Repository/CityRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarWatch.Data;
+using SolarWatch.Models;
+
+namespace SolarWatch.Services.CityServices.Repository;
+
+public class CityRepository(SolarDbContext context) : ICityRepository
+{
+    public void CreateCity(City city)
+    {
+        context.Cities.Add(city);
+        context.SaveChanges();
+    }
+
+    public IEnumerable<City> GetCities()
+    {
+        return context.Cities
+            .Include(c => c.SolarData);
+    }
+
+    public async ValueTask<City?> GetCityByName(string name)
+    {
+        return await context.Cities
+            .Include(c => c.SolarData)
+            .FirstOrDefaultAsync(c => c.Name == name);
+    }
+
+    public City UpdateCity(City city)
+    {
+        context.Update(city);
+        context.SaveChanges();
+        return new City
+        {
+            Id = city.Id, Name = city.Name, SolarData = city.SolarData,
+            Country = city.Country, 
+            Latitude = city.Latitude, Longitude = city.Longitude, 
+            State = city.State
+        };
+    }
+
+    public void DeleteCity(City city)
+    {
+        context.Cities.Remove(city);
+        context.SaveChanges();
+    }
+}
