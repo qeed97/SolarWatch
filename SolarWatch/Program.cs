0010b800e179ec6362fb7be352c73b4e1573a379
qeed97 <qeed97@gmail.com>
2024-09-30 19:41:33 +0200

    Registration services,contracts and controller created

--- SolarWatch/Program.cs
+++ SolarWatch/Program.cs
@@ -1,9 +1,11 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using SolarWatch.Data;
 using SolarWatch.Services;
+using SolarWatch.Services.Authentication;
 using SolarWatch.Services.CityServices.Repository;
 using SolarWatch.Services.Jsonprocessor;
 using SolarWatch.Services.SolarDataServices.Repository;
@@ -20,6 +22,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<SolarDbContext>();
 builder.Services.AddScoped<ICityRepository, CityRepository>();
 builder.Services.AddScoped<ISolarDataRepository, SolarDataRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddSingleton<ILocationDataProvider, OpenWeatherMapApi>();
 builder.Services.AddSingleton<ILocationJsonProcessor, LocationJsonProcessor>();
 builder.Services.AddSingleton<ISolarDataProvider, SolarDataProvider>();
@@ -47,6 +50,21 @@ builder.Services.AddDbContext<UsersContext>(options =>
     options.UseSqlServer(
         $"Server=solar-watch,1433;Database=solar-watch;User Id=sa;Password=Hurkakolbasz24?;Encrypt=false;");
 });
+
+builder.Services
+    .AddIdentityCore<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    })
+    .AddEntityFrameworkStores<UsersContext>();
+
+
 
 var app = builder.Build();
 
